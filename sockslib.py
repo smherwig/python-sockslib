53ffe0e2bb2e60627eb462ac228108a4384976f3
smherwig <smherwig@cs.umd.edu>
2019-04-17 10:27:09 -0400

    Add source, sink, middleware (filter) abstraction.
    The idea is that the SocksServerEndpoint is a source
    and the AppClientEndpoint is the sink, and various filters
    can be placed between the two.

--- sockslib.py
+++ sockslib.py
@@ -8,6 +8,7 @@ import sys
 import ringbuffer
 
 BLOCK_SIZE = 8192
+BUF_SIZE = 8192
 
 SOCKS_STATE_CLIENT_HELLO            = 1
 SOCKS_STATE_CLIENT_HELLO_DOMAINNAME = 2
@@ -17,9 +18,10 @@ SOCKS_STATE_SERVER_HELLO_CLOSE      = 5
 SOCKS_STATE_RELAY                   = 6
 SOCKS_STATE_CLOSE                   = 7
 
-APP_STATE_CONNECTING = 1
-APP_STATE_CONNECTED  = 2
-APP_STATE_CLOSED     = 3
+ENDPOINT_STATE_NEW        = 1
+ENDPOINT_STATE_CONNECTING = 2
+ENDPOINT_STATE_CONNECTED  = 3
+ENDPOINT_STATE_CLOSED     = 4
 
 SOCKS_CLIENT_HELLO_MIN_LENGTH = 9
 SOCKS_SERVER_HELLO_LENGTH = 8
@@ -30,63 +32,94 @@ SOCKS_CMD_BIND      = 0x02
 SOCKS_REQUEST_GRANTED = 0x5a
 SOCKS_REQUEST_DENIED = 0x5b
 
-class AppClientEndpoint(asyncore.dispatcher):
-    def __init__(self, host, port, socks_srv):
-        print 'here'
+# outbound (out) means data going from application client to application server
+#
+# inbound  (in) means data going from application server to application client
+#
+# upstream means closer to the application client
+#
+# downstream means closer to the application server
+
+
+class SourceNode:
+    def __init__(self, buf_size=BUF_SIZE):
+        # a downstream node writes incoming data here
+        self.in_ringbuf = ringbuffer.RingBuffer(buf_size)
+        self.upstream_state = ENDPOINT_STATE_NEW
+
+class MiddleNode:
+    def __init__(self, out_buf_size=BUF_SIZE, in_buf_size=BUF_SIZE):
+        # an downstream node writes incoming data here
+        self.in_ringbuf = rinbuffer.RingBuffer(in_buf_size)
+        # an upstream node writes outgoing data here
+        self.out_ringbuf = ringbuffer.RingBuffer(out_buf_size)
+        self.upstream_state = ENDPOINT_STATE_NEW
+        self.downstream_state = ENDPOINT_STATE_NEW
+
+class SinkNode:
+    def __init__(self, host, port, buf_size=BUF_SIZE):
+        # an upstream node writes outgoing data here
+        self.state = ENDPOINT_STATE_NEW
+        self.out_ringbuf = ringbuffer.RingBuffer(buf_size)
+        self.downstream_state = ENDPOINT_STATE_NEW
+        self.host = host
+        self.port = port
+
+class AppClientEndpoint(asyncore.dispatcher, SinkNode):
+    def __init__(self, host, port, upstream_node):
         asyncore.dispatcher.__init__(self)
-        # The data from we read from the app_server is
-        # written here; the SOCKSEndpoint then reads from the
-        # ringbuf when relaying to the app_client.
-        self.ringbuf = ringbuffer.RingBuffer(BLOCK_SIZE * 2)
-        self.socks_srv = socks_srv
+        SinkNode.__init__(self, host, port)
+        self.upstream = upstream_node
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.state = APP_STATE_CONNECTING
+        self.downstream_state = ENDPOINT_STATE_CONNECTING
         try:
             self.connect((host, port))
         except socket.error as e:
-            self.state = APP_STATE_CLOSED
+            self.upstream_state = ENDPOINT_STATE_CLOSED
             self.close()
 
     def _relay_to_app_server(self):
         print 'app: relay_to_app_server'
-        n = self.socks_srv.ringbuf.avail_read()
+        n = self.out_ringbuf.avail_read()
         m = min(BLOCK_SIZE, n)
         if m > 0:
-            data = self.socks_srv.ringbuf.peek(m)
+            data = self.out_ringbuf.peek(m)
             print 'app: relay_to_app_server: n=%d, m=%d, data="%s"' % (n, m, data)
             nsent = self.send(data)
             print 'app: relay_to_app_server: want %d, sent %d' % (m, nsent)
             if nsent:
-                _ = self.socks_srv.ringbuf.read(nsent)
+                _ = self.out_ringbuf.read(nsent)
 
-    def _relay_to_socks_server(self):
-        print 'app: relay_to_socks_server'
-        n = self.ringbuf.avail_write()
+    def _relay_upstream(self):
+        print 'app: _relay_upstreamr'
+        n = self.upstream.in_ringbuf.avail_write()
         m = min(BLOCK_SIZE, n)
         data = self.recv(m)
         if not data:
             return
-        self.ringbuf.write(data)
+        self.upstream.in_ringbuf.write(data)
 
     def readable(self):
-        if self.state == APP_STATE_CONNECTING:
+        if self.downstream_state == ENDPOINT_STATE_CONNECTING:
             return True
-        elif self.state == APP_STATE_CONNECTED and self.ringbuf.avail_write() > 0:
+        elif self.downstream_state == ENDPOINT_STATE_CONNECTED and \
+                self.upstream.in_ringbuf.avail_write() > 0:
             return True
         else:
             return False
     
     def writable(self):
-        if self.state == APP_STATE_CONNECTING:
+        if self.downstream_state == ENDPOINT_STATE_CONNECTING:
             return True
-        elif self.state == APP_STATE_CONNECTED and self.socks_srv.ringbuf.avail_read() > 0:
+        elif self.downstream_state == ENDPOINT_STATE_CONNECTED and \
+                self.out_ringbuf.avail_read() > 0:
             return True
         else:
             return False
 
     def handle_connect(self):
         print 'app: handle_connect'
-        self.state = APP_STATE_CONNECTED
+        self.downstream_state = ENDPOINT_STATE_CONNECTED
 
     def handle_error(self):
         print 'app: error'
@@ -94,38 +127,38 @@ class AppClientEndpoint(asyncore.dispatcher):
 
     def handle_close(self):
         print 'app: close'
-        self.state = APP_STATE_CLOSED
+        self.downstream_state = ENDPOINT_STATE_CLOSED
         self.close()
 
     def handle_read(self):
-        if self.state == APP_STATE_CONNECTED:
-            self._relay_to_socks_server()
+        if self.downstream_state == ENDPOINT_STATE_CONNECTED:
+            self._relay_upstream()
 
     def handle_write(self):
-        if self.state == APP_STATE_CONNECTED:
+        if self.downstream_state == ENDPOINT_STATE_CONNECTED:
             self._relay_to_app_server()
 
 
-class SOCKSServerEndpoint(asyncore.dispatcher):
-    def __init__(self, sock, app_class=AppClientEndpoint):
+class SOCKSServerEndpoint(asyncore.dispatcher, SourceNode):
+    def __init__(self, sock, downstream_class=AppClientEndpoint):
         asyncore.dispatcher.__init__(self, sock)
-        self.app_class = app_class
+        SourceNode.__init__(self)
+        self.downstream_class = downstream_class
+        self.downstream = None
         # used for the client handshake
         self.hsbuf = bytearray()
         # data we read from app_client, headed toward the app_server
-        self.ringbuf = ringbuffer.RingBuffer(BLOCK_SIZE * 2)
+        self.out_ringbuf = ringbuffer.RingBuffer(BLOCK_SIZE * 2)
         self.state = SOCKS_STATE_CLIENT_HELLO
         self.socks_version = None
         self.socks_command = None
         self.user = None
-        self.app = None
         self.app_port = None
         self.app_ipstr = None
         self.app_domain = None
 
     def _make_server_hello(self, status):
         self.hsbuf = struct.pack('BBHI', 0, status, 0, 0)
-
 
     def _recv_client_hello_domainname(self):
         print 'srv: recv_client_hello_domainname'
@@ -140,7 +173,7 @@ class SOCKSServerEndpoint(asyncore.dispatcher):
             return
         self.app_domain = self.hsbuf[:domain_end]
         self.state = SOCKS_STATE_WAIT_CONNECT
-        self.app = app_class(self.app_domain, self.app_port, self)
+        self.downstream = self.downstream_class(self.app_domain, self.app_port, self)
 
     def _recv_client_hello(self):
         print 'srv: recv_client_hello'
@@ -177,10 +210,10 @@ class SOCKSServerEndpoint(asyncore.dispatcher):
                     self.app_domain = str(self.hsbuf[:domain_end])
                     print self.app_domain
                     self.state = SOCKS_STATE_WAIT_CONNECT
-                    self.app = self.app_class(self.app_domain, self.app_port, self)
+                    self.downstream = self.downstream_class(self.app_domain, self.app_port, self)
             else:
                 self.state = SOCKS_STATE_WAIT_CONNECT
-                self.app = self.app_class(self.app_ipstr, self.app_port, self)
+                self.downstream = self.downstream_class(self.app_ipstr, self.app_port, self)
 
     def _send_server_hello_open(self):
         print 'srv: send_server_hello_open'
@@ -197,29 +230,30 @@ class SOCKSServerEndpoint(asyncore.dispatcher):
             self.close()
         self.hsbuf = self.hsbuf[n:]
 
-    def _relay_to_app_server(self):
-        print 'srv: relay_to_app_server'
-        n = self.ringbuf.avail_write()
+    def _relay_downstream(self):
+        print 'srv: _relay_downstream'
+        ringbuf = self.downstream.out_ringbuf
+        n = ringbuf.avail_write()
         m = min(BLOCK_SIZE, n)
         data = self.recv(m)
-        print 'srv: relay_to_app_server: want %d, got %d' % (m, len(data))
         if not data:
             return
-        self.ringbuf.write(data)
+        ringbuf.write(data)
 
     def _relay_to_app_client(self):
-        print 'srv: relay_to_app_client'
-        n = self.app.ringbuf.avail_read()
+        print 'srv: _relay_to_app_client'
+        ringbuf = self.in_ringbuf
+        n = ringbuf.avail_read()
         m = min(BLOCK_SIZE, n)
-        data = self.app.ringbuf.peek(m)
+        data = ringbuf.peek(m)
         nsent = self.send(data)
         if nsent:
-            _ = self.app.ringbuf.read(nsent)
+            _ = ringbuf.read(nsent)
 
     def readable(self):
         if self.state in (SOCKS_STATE_CLIENT_HELLO, SOCKS_STATE_CLIENT_HELLO_DOMAINNAME):
             return True
-        elif self.state == SOCKS_STATE_RELAY and self.ringbuf.avail_write() > 0:
+        elif self.state == SOCKS_STATE_RELAY and self.downstream.out_ringbuf.avail_write() > 0:
             return True
         else:
             return False
@@ -228,7 +262,7 @@ class SOCKSServerEndpoint(asyncore.dispatcher):
         if self.state in (SOCKS_STATE_WAIT_CONNECT,
                 SOCKS_STATE_SERVER_HELLO_CLOSE, SOCKS_STATE_SERVER_HELLO_OPEN):
             return True
-        elif self.state == SOCKS_STATE_RELAY and self.app.ringbuf.avail_read() > 0:
+        elif self.state == SOCKS_STATE_RELAY and self.in_ringbuf.avail_read() > 0:
             return True
         else:
             return False
@@ -244,22 +278,21 @@ class SOCKSServerEndpoint(asyncore.dispatcher):
         elif self.state == SOCKS_STATE_CLIENT_HELLO_DOMAINNAME:
             self._recv_client_hello_domainname()
         elif self.state == SOCKS_STATE_RELAY:
-            self._relay_to_app_server()
+            self._relay_downstream()
 
     def handle_write(self):
         print 'srv: handle_write'
         if self.state == SOCKS_STATE_WAIT_CONNECT:
-            if self.app.state == APP_STATE_CONNECTED:
+            if self.downstream.downstream_state == ENDPOINT_STATE_CONNECTED:
                 self.state = SOCKS_STATE_SERVER_HELLO_OPEN
                 self._make_server_hello(SOCKS_REQUEST_GRANTED)
                 self._send_server_hello_open()
-            elif self.app.state == APP_STATE_CLOSED:
+            elif self.downstream.downstream_state == ENDPOINT_STATE_CLOSED:
                 self.state = SOCKS_STATE_SERVER_HELLO_CLOSE
                 self._make_server_hello(SOCKS_REQUEST_DENIED)
                 self._send_server_hello_close()
         elif self.state == SOCKS_STATE_RELAY:
             self._relay_to_app_client()
-
 
 class SOCKSServer(asyncore.dispatcher):
     def __init__(self, host, port):
